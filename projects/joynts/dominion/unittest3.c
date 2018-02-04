14f699b0cd5d34098f57edbe26be34e035948a77
joynts <joynts@oregonstate.edu>
2018-02-04 13:00:32 -0500

    Create unittest3.c

---
+++ projects/joynts/dominion/unittest3.c
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+// Scott Joynt
+// CS 362
+// Unit Test 3
+// -----------------------------------------------------------------------
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1

ecfcde4128101e6aabacd5ea3c16b8947e303165
joynts <joynts@oregonstate.edu>
2018-02-04 15:48:52 -0500

    Update cardtest1.c

--- projects/joynts/dominion/cardtest1.c
+++ projects/joynts/dominion/cardtest1.c
@@ -1,146 +1,84 @@
-/*
- * cardtest4.c
- *
- 
- */
-
-/*
- * Include the following lines in your makefile:
- *
- * cardtest4: cardtest4.c dominion.o rngs.o
- *      gcc -o cardtest1 -g  cardtest4.c dominion.o rngs.o $(CFLAGS)
- */
-
-
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
-#include <stdlib.h>
+#include <math.h>
 
-#define TESTCARD "steward"
+int SmithyTEST(struct gameState *post, int handPos);
 
-int main() {
-    int newCards = 0;
-    int discarded = 1;
-    int xtraCoins = 0;
-    int shuffledCards = 0;
-
-    int i, j, m;
-    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int remove1, remove2;
-    int seed = 1000;
-    int numPlayers = 2;
-    int thisPlayer = 0;
-	struct gameState G, testG;
-	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-			sea_hag, tribute, smithy, council_room};
-
-	// initialize a game state and player cards
-	initializeGame(numPlayers, k, seed, &G);
-
-	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
-
-	// ----------- TEST 1: choice1 = 1 = +2 cards --------------
-	printf("TEST 1: choice1 = 1 = +2 cards\n");
-
-	// copy the game state to a test case
-	memcpy(&testG, &G, sizeof(struct gameState));
-	choice1 = 1;
-	cardEffect(steward, choice1, choice2, choice3, &testG, handpos, &bonus);
-
-	newCards = 2;
-	xtraCoins = 0;
-	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
-	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
-	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
-	assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
-	assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
-	assert(testG.coins == G.coins + xtraCoins);
-
-	// ----------- TEST 2: choice1 = 2 = +2 coins --------------
-	printf("TEST 2: choice1 = 2 = +2 coins\n");
-
-	// copy the game state to a test case
-	memcpy(&testG, &G, sizeof(struct gameState));
-	choice1 = 2;
-	cardEffect(steward, choice1, choice2, choice3, &testG, handpos, &bonus);
-
-	newCards = 0;
-	xtraCoins = 2;
-	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
-	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
-	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
-	assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
-	assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
-	assert(testG.coins == G.coins + xtraCoins);
-
-	// ----------- TEST 3: choice1 = 3 = trash two cards --------------
-
-	printf("TEST 3: choice1 = 3 = trash two cards\n");
-	choice1 = 3;
-
-	// cycle through each eligible combination of 2 cards to trash
-	for (i=1; i<G.handCount[thisPlayer]; i++) {
-		for (j=i+1; j<G.handCount[thisPlayer]; j++) {
-
-			G.hand[thisPlayer][0] = steward;
-			G.hand[thisPlayer][1] = copper;
-			G.hand[thisPlayer][2] = duchy;
-			G.hand[thisPlayer][3] = estate;
-			G.hand[thisPlayer][4] = feast;
-
-			// copy the game state to a test case
-			memcpy(&testG, &G, sizeof(struct gameState));
-
-			printf("starting cards: ");
-			for (m=0; m<testG.handCount[thisPlayer]; m++) {
-				printf("(%d)", testG.hand[thisPlayer][m]);
-			}
-			printf("; ");
-
-			choice2 = j;
-			choice3 = i;
-			remove1 = testG.hand[thisPlayer][i];
-			remove2 = testG.hand[thisPlayer][j];
-			cardEffect(steward, choice1, choice2, choice3, &testG, handpos, &bonus);
-
-			printf("removed: (%d)(%d); ", remove1, remove2);
-			printf("ending cards: ");
-
-			// tests that the removed cards are no longer in the player's hand
-			for (m=0; m<testG.handCount[thisPlayer]; m++) {
-				printf("(%d)", testG.hand[thisPlayer][m]);
-				assert(testG.hand[thisPlayer][m] != remove1);
-				assert(testG.hand[thisPlayer][m] != remove2);
-			}
-			printf(", expected: ");
-			for (m=1; m<G.handCount[thisPlayer]; m++) {
-				if (G.hand[thisPlayer][m] != G.hand[thisPlayer][i] && G.hand[thisPlayer][m] != G.hand[thisPlayer][j]) {
-					printf("(%d)", G.hand[thisPlayer][m]);
-				}
-			}
-			printf("\n");
-
-			// tests for the appropriate number of remaining cards
-			newCards = 0;
-			xtraCoins = 0;
-			discarded = 3;
-			if (i==1 && j==2) {
-				printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
-				printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
-			}
-			assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
-			assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
+int main(){
+	int player = 0;
+	struct gameState postGameState;
+	int i, j, k, l, m, n, handPos;
+	
+	for (i = 0; i < sizeof(struct gameState); i++) { 
+		((char*)&postGameState)[i] = floor(Random() * 256);
+	}
+	
+	SelectStream(2);
+	PutSeed(3);
+	printf ("START - Running callSmithyCard().\n\n");
+	
+	postGameState.whoseTurn = player;
+	for(k = 0; k < 500; k++){
+		postGameState.handCount[player] = floor(Random() * MAX_HAND)+1;
+		postGameState.deckCount[player] = floor(Random() * MAX_DECK);
+		postGameState.discardCount[player] = floor(Random() * MAX_DECK);
+		postGameState.playedCardCount = floor(Random() * MAX_DECK);
+		
+		for(l = 0; l < postGameState.handCount[player]; l++){
+			postGameState.hand[player][l] = floor(Random() * treasure_map) + 1;
+		}
+		
+		for(j = 0; j < postGameState.discardCount[player]; j++){
+			postGameState.discard[player][j] = floor(Random() * treasure_map) + 1;
 		}
 
+		for(m = 0; m < postGameState.deckCount[player]; m++)	{
+			postGameState.deck[player][m] = floor(Random() * treasure_map) + 1;
+		}
+		for(n = 0; n < postGameState.playedCardCount; n++){
+			postGameState.playedCards[n] = floor(Random() * treasure_map) + 1;
+		}
+		
+		handPos = floor(Random() * postGameState.handCount[player]);
+		postGameState.hand[player][handPos] = smithy;
+		
+		SmithyTEST(&postGameState, handPos);
 	}
-
-	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
-
-
+	
+	printf ("STOP - Running callSmithyCard().\n\n");
 	return 0;
 }
 
+int SmithyTEST(struct gameState *post, int handPos){
+	int player = post->whoseTurn;
+	struct gameState preGameState;
+	memcpy(&preGameState, post, sizeof(struct gameState));
+	
+	callSmithyCard(post, player, handPos);
+	
+	preGameState.handCount[player] = preGameState.handCount[player] + 2;
+	preGameState.playedCardCount++;
+	
+	if(preGameState.deckCount[player] < 3){
+		preGameState.deckCount[player] = preGameState.deckCount[player] + preGameState.discardCount[player] - 3;
+	}
+	else{
+		preGameState.deckCount[player] = preGameState.deckCount[player] - 3;
+	}
+	
+	if(post->handCount[player] != preGameState.handCount[player]){
+		printf("ERROR! handCount is incorrect.\n");
+	}
+	if(post->deckCount[player] != preGameState.deckCount[player]){
+		printf("ERROR! deckCount is incorrect.\n");
+	}
+	if(post->playedCardCount != preGameState.playedCardCount){
+		printf("ERROR! playedCardCount is incorrect.\n");
+	}
+	
+	return 0;
+}

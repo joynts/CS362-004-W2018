aecfd647cd188039b8a2bc04cc2fe24db34eb93c
joynts <joynts@oregonstate.edu>
2018-02-04 15:58:11 -0500

    Update cardtest1.c

--- projects/joynts/dominion/cardtest1.c
+++ projects/joynts/dominion/cardtest1.c
@@ -1,84 +1,59 @@
+// -----------------------------------------------------------------------
+// Scott Joynt
+// CS 362
+// Card Test 4
+// -----------------------------------------------------------------------
+
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
-#include <math.h>
 
-int SmithyTEST(struct gameState *post, int handPos);
-
-int main(){
-	int player = 0;
-	struct gameState postGameState;
-	int i, j, k, l, m, n, handPos;
-	
-	for (i = 0; i < sizeof(struct gameState); i++) { 
-		((char*)&postGameState)[i] = floor(Random() * 256);
-	}
-	
-	SelectStream(2);
-	PutSeed(3);
-	printf ("START - Running callSmithyCard().\n\n");
-	
-	postGameState.whoseTurn = player;
-	for(k = 0; k < 500; k++){
-		postGameState.handCount[player] = floor(Random() * MAX_HAND)+1;
-		postGameState.deckCount[player] = floor(Random() * MAX_DECK);
-		postGameState.discardCount[player] = floor(Random() * MAX_DECK);
-		postGameState.playedCardCount = floor(Random() * MAX_DECK);
-		
-		for(l = 0; l < postGameState.handCount[player]; l++){
-			postGameState.hand[player][l] = floor(Random() * treasure_map) + 1;
-		}
-		
-		for(j = 0; j < postGameState.discardCount[player]; j++){
-			postGameState.discard[player][j] = floor(Random() * treasure_map) + 1;
-		}
-
-		for(m = 0; m < postGameState.deckCount[player]; m++)	{
-			postGameState.deck[player][m] = floor(Random() * treasure_map) + 1;
-		}
-		for(n = 0; n < postGameState.playedCardCount; n++){
-			postGameState.playedCards[n] = floor(Random() * treasure_map) + 1;
-		}
-		
-		handPos = floor(Random() * postGameState.handCount[player]);
-		postGameState.hand[player][handPos] = smithy;
-		
-		SmithyTEST(&postGameState, handPos);
-	}
-	
-	printf ("STOP - Running callSmithyCard().\n\n");
-	return 0;
-}
-
-int SmithyTEST(struct gameState *post, int handPos){
-	int player = post->whoseTurn;
-	struct gameState preGameState;
-	memcpy(&preGameState, post, sizeof(struct gameState));
-	
-	smithy_card(post, player, handPos);
-	
-	preGameState.handCount[player] = preGameState.handCount[player] + 2;
-	preGameState.playedCardCount++;
-	
-	if(preGameState.deckCount[player] < 3){
-		preGameState.deckCount[player] = preGameState.deckCount[player] + preGameState.discardCount[player] - 3;
-	}
-	else{
-		preGameState.deckCount[player] = preGameState.deckCount[player] - 3;
-	}
-	
-	if(post->handCount[player] != preGameState.handCount[player]){
-		printf("ERROR! handCount is incorrect.\n");
-	}
-	if(post->deckCount[player] != preGameState.deckCount[player]){
-		printf("ERROR! deckCount is incorrect.\n");
-	}
-	if(post->playedCardCount != preGameState.playedCardCount){
-		printf("ERROR! playedCardCount is incorrect.\n");
-	}
-	
-	return 0;
+int main() {
+    int seed = rand() % 1000;
+    int numPlayers = 2;
+    int p, player, round;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+        , remodel, smithy, village, baron, great_hall};
+    struct gameState oldGame;
+    struct gameState newGame;
+    int maxHandCount = 5;
+    int failCount = 0;
+    int passCount = 0;
+    
+    printf ("Testing: Smithy\n");
+    
+    for(p = 0; p < maxHandCount; p++)
+    {
+        memset(&oldGame, 23, sizeof(struct gameState));
+        memset(&newGame, 23, sizeof(struct gameState));         
+        initializeGame(numPlayers, k, seed, &newGame);
+        for(round = 0; round < 2; round++)
+        {
+            for(player = 0; player < numPlayers; player++)
+            {
+                memcpy(&oldGame, &newGame, sizeof(struct gameState));
+                smithy_card(smithy, player, &newGame);
+                
+                printf("Player %d\n", player);
+                printf("Old - deckCount: %d handCount: %d\n", oldGame.deckCount[player], oldGame.handCount[player]);
+                printf("New - deckCount: %d handCount: %d\n", newGame.deckCount[player], newGame.handCount[player]);
+                
+                if((newGame.handCount[player] == oldGame.handCount[player] + 3) && (newGame.deckCount[player] == oldGame.deckCount[player] - 3))
+                {
+                    passCount++;
+                }
+                else
+                {
+                    failCount++;
+                }
+            }
+        }
+    }
+    printf("Number of cases passed: %d\n", passCount);
+    printf("Number of cases failed: %d\n", failCount);
+    
+    return 0;
 }
